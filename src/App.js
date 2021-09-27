789f09fccb3fef40ff7018abab91ba20a165f3f0
psiisyapk <psiisyapk@users.noreply.github.com>
2021-09-27 15:17:57 +0300

    homework 02

--- src/App.js
+++ src/App.js
@@ -1,17 +1,76 @@
 import './App.css';
-import Message from "./Components/Message";
-import React, {Component} from 'react'
+import React, {useEffect, useState} from "react";
 
-let text = "MESSAGE TEXT"
+export function App() {
+    const [submitForm, ChangeState] = useState(false)
+    const [messageList, setValue] = useState([
+        {
+            "text": "", "author": ""
+        }
+        ]);
 
-class App extends Component {
-  render() {
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            function sendMessage(inputText, author) {
+                const updateArray = [...messageList];
+                updateArray.push({author: author, text: inputText})
+                setValue(updateArray);
+            }
+            if (submitForm === true) {
+                sendMessage("Ваше сообщение принято", "robot")
+                ChangeState(false);
+            }
+        }, 1500)
+        return () => clearInterval(intervalId);
+    }, [submitForm, messageList]);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        let inputText = event.target[0].value
+        if (inputText !== '') {
+            const updateArray = [...messageList];
+            updateArray.push({author: "human", text: inputText})
+            setValue(updateArray);
+            ChangeState(true);
+        }
+    }
+
+    function Form() {
+        return (
+            <form onSubmit={handleSubmit}>
+                <input type="text" />
+                <input type="submit" value="Go" />
+            </form>
+        )
+    }
+
+    function Table() {
+        return (
+            <table>
+                <tbody>
+                <tr>
+                    <th>Author</th>
+                    <th>Text</th>
+                </tr>
+                {messageList.map( (data, index) =>
+                    <tr key={index} style={{ display: data.text ? "row" : "none" }}>
+                        <td style={{ display: data.text ? "row" : "none" }}>
+                            {data.author}
+                        </td>
+                        <td style={{ display: data.text ? "row" : "none" }}>
+                            {data.text}
+                        </td>
+                    </tr>
+                )}
+                </tbody>
+            </table>
+        )
+    }
+
     return (
-        <div className="App">
-          <Message text={text}/>
+        <div>
+            < Form/>
+            < Table/>
         </div>
     )
-  }
 }
-
-export default App
