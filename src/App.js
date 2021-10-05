5fa9d2c47948de884a19e4848717bb52064a7af1
psiisyapk <psiisyapk@users.noreply.github.com>
2021-10-05 17:47:50 +0300

    homework 04

--- src/App.js
+++ src/App.js
@@ -1,137 +1,89 @@
-import './App.css';
-import React, {useEffect, useState, useRef } from "react";
-import MessageIcon from '@mui/icons-material/Message';
-import { DataGrid } from '@mui/x-data-grid';
-import {
-    Card, CardContent, FormControl, Button, Grid, List, ListItem, ListItemText, ListSubheader, TextField
-} from '@mui/material';
+import {BrowserRouter, NavLink, Route, Switch} from "react-router-dom";
+import {List, ListItemButton} from "@mui/material";
+import Profile from "./Routes/Profile";
+import Chats from "./Routes/Chats";
+import Home from "./Routes/Home";
+import NoChat from "./Routes/NoChat";
+import React from "react";
+import {AppBar, Box, CssBaseline, Divider, Drawer, Toolbar} from "@material-ui/core";
 
-export function App() {
-    const [submitForm, ChangeState] = useState(false)
-    const inputRef = useRef(null);
-    const [messageList, setValue] = useState([
-        {
-            "text": "", "author": ""
-        }
-        ]);
-    const ChatList = [
-        {"id": "001", "name": "Chat1"},
-        {"id": "002", "name": "Chat2"},
-        {"id": "003", "name": "Chat3"},
-        {"id": "004", "name": "Chat4"},
-        {"id": "005", "name": "Chat5"},
-        {"id": "006", "name": "Chat6"},
-        {"id": "007", "name": "Chat7"},
-        {"id": "008", "name": "Chat8"},
-        {"id": "009", "name": "Chat9"},
-        {"id": "0010", "name": "Chat10"},
-    ]
-    useEffect(() => {
-        inputRef.current.focus();
-    }, []);
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            function sendMessage(inputText, author) {
-                const updateArray = [...messageList];
-                updateArray.push({author: author, text: inputText})
-                setValue(updateArray);
-            }
-            if (submitForm === true) {
-                sendMessage("Ваше сообщение принято", "robot")
-                ChangeState(false);
-                inputRef.current.focus();
-            }
-        }, 1500)
-        return () => clearInterval(intervalId);
-    }, [submitForm, messageList]);
-
-    function handleSubmit(event) {
-        event.preventDefault();
-        let inputText = event.target[0].value
-        if (inputText !== '') {
-            const updateArray = [...messageList];
-            updateArray.push({author: "human", text: inputText})
-            setValue(updateArray);
-            ChangeState(true);
-        }
-    }
-
-    function Chats() {
-        return (
-            <List sx={{
-                width: '100%',
-                maxWidth: 360,
-                color: 'text.secondary',
-                border: 1,
-                borderColor: 'gray',
-                borderRadius: 2,
-            }}
-                  component="nav"
-                  aria-labelledby="nested-list-subheader"
-                  subheader={
-                      <ListSubheader component="div" id="nested-list-subheader">
-                          Chat list
-                      </ListSubheader>
-                  }
-                  className="ChatListClass"
-            >
-                {ChatList.map((data) =>
-                    <ListItem key={data.id} className="ListItem">
-                        <ListItemText primary={`${data.name}`} /><MessageIcon/>
-                    </ListItem>
-                )}
-            </List>
-        )
-    }
-
-    function Form() {
-        return (
-            <Card sx={{ minWidth: 275, margin: 5, borderColor: 'grey.500', height: 100, boxShadow: 7}}>
-                <CardContent>
-                    <form onSubmit={handleSubmit} className="FormClass">
-                        <Grid container spacing={2} className="GridForm">
-                            <Grid item xs={3}>
-                                <FormControl fullWidth >
-                                    <TextField type="text" inputRef={inputRef} label="Enter message"
-                                           className="inputClass" variant="standard"/>
-                                </FormControl>
-                            </Grid>
-                            <Grid item xs={2} className="GridButton">
-                                <Button variant="contained" type="submit" fullWidth className="buttonForm">Go</Button>
-                            </Grid>
-                        </Grid>
-                    </form>
-                </CardContent></Card>
-        )
-    }
-
-    function GridMessages() {
-        return (
-            <div style={{ height: 800, width: '95%' }} className="GridMessages">
-                <DataGrid
-                    columns={[
-                        { field: 'Author', width: 200},
-                        { field: 'Text', width: 700}
-                    ]}
-                    rows={messageList.filter(v => v.text !== "").map((data, index) =>
-                        ({
-                            Text: data.text,
-                            Author: data.author,
-                            id: index
-                        })
-                    )}
-                />
-            </div>
-        )
-    }
-
+function App() {
     return (
-        <div>
-            <div className="chat_ws"><Chats/></div>
-            <div className="user_ws">
-                <div><Form/></div>
-                <div><GridMessages/></div>
-            </div>
-        </div>
+        <BrowserRouter>
+            <Header/>
+            <Switch>
+                <Route path="/profile">
+                    <Profile />
+                </Route>
+                <Route exact path="/chats">
+                    <Chats />
+                </Route>
+                <Route path="/chats/:chatId">
+                    <Chats />
+                </Route>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/nochat">
+                    <NoChat />
+                </Route>
+                <Route>
+                    <h3 style={{ marginLeft: 100 }}>Page not found</h3>
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+function Header () {
+    const [selectedIndex, setSelectedIndex] = React.useState(0);
+    const handleListItemClick = (index) => {
+        setSelectedIndex(index);
+    };
+    const drawerWidth = 240;
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar
+                position="fixed"
+                sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}>
+            </AppBar>
+            <Drawer
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                        boxSizing: 'border-box',
+                    },
+                }}
+                variant="permanent"
+                anchor="left">
+                <Toolbar />
+                <Divider />
+                <header>
+                    <List>
+                        <ListItemButton selected={selectedIndex === 0} className="ListItemButtonClass"
+                                        style={{ backgroundColor: selectedIndex ===0 ? "lightblue" : "transparent"}}
+                                        onClick={() => handleListItemClick(0)}>
+                            <NavLink to="/" style={{ textDecoration: 'none' }}>Home</NavLink>
+                        </ListItemButton>
+                        <ListItemButton selected={selectedIndex === 1} className="ListItemButtonClass"
+                                        style={{ backgroundColor: selectedIndex ===1 ? "lightblue" : "transparent"}}
+                                        onClick={() => handleListItemClick(1)}>
+                            <NavLink to="/profile" style={{ textDecoration: 'none' }}>Profile</NavLink>
+                        </ListItemButton>
+                        <ListItemButton selected={selectedIndex === 2} className="ListItemButtonClass"
+                                        style={{ backgroundColor: selectedIndex ===2 ? "lightblue" : "transparent"}}
+                                        onClick={() => handleListItemClick(2)}>
+                            <NavLink to="/chats" style={{ textDecoration: 'none' }}>Chats</NavLink>
+                        </ListItemButton>
+                    </List>
+                </header>
+            </Drawer>
+        </Box>
+
     )
 }
+
+export default App;
